6dee76919cfb29f04b48ae42eaa3cfd836d3ed6e
R. Comonfort <a.comonfort@outlook.com>
2018-10-28 20:28:08 -0600

    Ready part 4 of swagger tutorial

---
+++ api/controllers/gamesystem.controller.js
@@ -0,0 +1,129 @@
+'use strict';
+
+var _ = require('lodash');
+
+var controllerHelper = require('../helpers/controller.helper');
+var messageHelper = require('../helpers/message.helper');
+var gamesystemService = require('../services/gamesystem.service');
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+// Module Name
+const MODULE_NAME = '[GameSystem Controller]';
+
+// Error Messages
+const GS_CT_ERR_GAMESYSTEM_NOT_FOUND = 'Gamesystem not found';
+
+// Success Messages
+const GS_CT_DELETED_SUCCESSFULLY = 'Gamesystem deleted successfully';
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+function getGameSystems(req, res) {
+
+  try {
+    // Receiving parameters
+    var params = {
+      name: req.swagger.params.name.value,
+      sort: req.swagger.params.sort.value
+    };
+
+    // Call to service
+    var result = gamesystemService.getGameSystems(params);
+
+    // Returning the result
+    res.json(result);
+  } catch (error) {
+    controllerHelper.handleErrorResponse(MODULE_NAME, getGameSystems.name, error, res);
+  }
+}
+
+function getGameSystemById(req, res) {
+
+  try {
+    var params = {
+      id: req.swagger.params.id.value
+    };
+
+    var result = gamesystemService.getGameSystemById(params.id);
+
+    if (!_.isUndefined(result)) {
+      res.json(result);
+    } else {
+      res.status(404).json(messageHelper.buildMessage(GS_CT_ERR_GAMESYSTEM_NOT_FOUND))
+    }
+  } catch (error) {
+    controllerHelper.handleErrorResponse(MODULE_NAME, getGameSystemById.name, error, res);
+  }
+}
+
+function createGameSystem(req, res) {
+
+  try {
+    var params = req.body;
+
+    var result = gamesystemService.createGameSystem(params);
+
+    if (!_.isUndefined(result) && _.isUndefined(result.error)) {
+      res.status(201).json(result);
+    } else {
+      res.status(409).json(messageHelper.buildMessage(result.error));
+    }
+  } catch (error) {
+    controllerHelper.handleErrorResponse(MODULE_NAME, createGameSystem.name, error, res);
+  }
+}
+
+function updateGameSystem(req, res) {
+
+  try {
+    var params = {
+      id: req.swagger.params.id.value
+    };
+    _.assign(params, req.body);
+
+    var result = gamesystemService.updateGameSystem(params);
+
+    if (!_.isUndefined(result) && _.isUndefined(result.error)) {
+      res.json(result);
+    } else {      
+      res.status(409).json(messageHelper.buildMessage(result.error));
+    }
+  } catch (error) {
+    controllerHelper.handleErrorResponse(MODULE_NAME, updateGameSystem.name, error, res);
+  }
+}
+
+function deleteGameSystem(req, res) {
+
+  try {
+    var params = {
+      id: req.swagger.params.id.value
+    };
+
+    var result = gamesystemService.deleteGameSystem(params.id);
+
+    if (!_.isUndefined(result) && _.isUndefined(result.error)) {
+      res.json(messageHelper.buildMessage(GS_CT_DELETED_SUCCESSFULLY));
+    } else {
+      res.status(404).json(messageHelper.buildMessage(result.error));
+    }
+  } catch (error) {
+    controllerHelper.handleErrorResponse(MODULE_NAME, createGameSystem.name, error, res);
+  }
+}
+
+module.exports = {
+  getGameSystems,
+  getGameSystemById,
+  createGameSystem,
+  updateGameSystem,
+  deleteGameSystem,
+  GS_CT_ERR_GAMESYSTEM_NOT_FOUND,
+  GS_CT_DELETED_SUCCESSFULLY,
+  MODULE_NAMe
+}
